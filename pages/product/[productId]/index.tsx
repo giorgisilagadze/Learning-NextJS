b53dc02f7117bce65de4e26d33b4120fac41123b
Giorgi Silagadze <pele@Giorgis-MacBook-Pro.local>
2023-06-22 23:40:59 +0400

    another changes

--- pages/product/[productId]/index.tsx
+++ pages/product/[productId]/index.tsx
@@ -1,10 +1,8 @@
 import { useRouter } from "next/router";
 import data from "../../../data.json";
 
-// export default function ProductId() {
-//   const route = useRouter();
-//   const producId = data.find((item) => item.id == route.query.productId);
-//   console.log(route.query.productId);
-
-//   return <h1>Product page of {producId?.text}</h1>;
-// }
+export default function ProductId() {
+  const route = useRouter();
+  const producId = data.find((item) => item.id == route.query.productId);
+  return <h1>Product page of {producId?.text}</h1>;
+}
