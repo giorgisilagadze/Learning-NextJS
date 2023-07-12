40383c1f0f1cdb823f17afbf42d1bc99c22afa77
Giorgi Silagadze <pele@Giorgis-MacBook-Pro.local>
2023-07-12 16:54:14 +0400

    POST request

--- pages/api/comments/index.ts
+++ pages/api/comments/index.ts
@@ -2,5 +2,15 @@ import { comments } from "@/data/comments";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function Comment(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(comments);
+  if (req.method === "GET") {
+    res.status(200).json(comments);
+  } else if (req.method === "POST") {
+    const comment = req.body.comm;
+    const newComment = {
+      id: Date.now(),
+      title: comment,
+    };
+    comments.push(newComment);
+    res.status(201).json(newComment);
+  }
 }
--- pages/comments/index.tsx
+++ pages/comments/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { json } from "stream/consumers";
 
 interface Item {
   id: number;
@@ -7,6 +8,7 @@ interface Item {
 
 export default function Comment() {
   const [comments, setComments] = useState([]);
+  const [comm, setComm] = useState("");
 
   const fetchData = async () => {
     const response = await fetch("/api/comments");
@@ -14,8 +16,21 @@ export default function Comment() {
     setComments(data);
   };
 
+  const submitData = async () => {
+    const response = await fetch("/api/comments", {
+      method: "Post",
+      body: JSON.stringify({ comm }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+  };
+
   return (
     <div>
+      <input type="text" onChange={(e) => setComm(e.target.value)} />
+      <button onClick={submitData}>submit data</button>
       <button onClick={fetchData}>fetch data</button>
       {comments.map((item: Item) => (
         <p>
