7c535b8acc5048ae3283e84e74c08aecca34cb3e
Giorgi Silagadze <pele@Giorgis-MacBook-Pro.local>
2023-07-16 20:54:29 +0400

    using Image component

--- pages/oldcars.tsx
+++ pages/oldcars.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
+import car from "../public/5.jpeg";
+
 export default function Oldcars() {
   return (
     <>
       <h1>old cars page</h1>
+      <Image src={car} placeholder="blur" alt="car" width="500" height="400" />
       {["1", "2", "3", "4", "5"].map((image) => (
-        <img src={`/${image}.jpeg`} alt="old car" width="300" height="200" />
+        <Image src={`/${image}.jpeg`} alt="old car" width="300" height="200" />
       ))}
     </>
   );
